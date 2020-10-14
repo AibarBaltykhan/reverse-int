abf28f58d1724f371c5a010eba092c63372e98ea
AibarBaltykhan <https://github.com/AibarBaltykhan>
2020-10-14 17:09:52 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -1 * n;
+    }
+    let str = n + '';
+    let revStr = ''
+    for (let i = str.length-1; i >= 0; i--) {
+        revStr += str[i];
+    }
+    let answer = 0;
+    answer += +revStr;
+    return answer;
 }
